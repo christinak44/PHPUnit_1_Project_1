568d184d37cc4523891331f218963020056b152d
Christina King <kingcm41@hotmail.com>
2019-08-15 13:38:54 -0400

    Final code
    project ready for submission w/ exceeds requirements included

--- a_random_quote_generator-v1/inc/functions.php
+++ a_random_quote_generator-v1/inc/functions.php
@@ -1,15 +1,15 @@
-
-
-
 <?php
-
+/* Auto refresh used in place of html code for load button; stackoverflow.com forum for the assist
+ <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>*/
 /*Create the Multidimensional array of quote elements and name it quotes
 Each inner array element should be an associative array
-My thoughts are to have all quote data in one area to keep "main" php file cleaner, dropping in this 'quote list' where needed (inside function(?) maybe)*/
+*/
+//'type' element added describes category or style of quote
 //Homer Simpson quotes from screenrant.com
 $quotes[] = array(
     'quote' => 'It takes two to lie; One to lie, and one to listen.',
     'source' => 'Homer Simpson',
+    'type' => 'Real Talk ala Mr. Simpson',
     'citation' => 'The Simpsons, Season 3, Ep 20',
     'year' => 1992
 
@@ -17,6 +17,7 @@ $quotes[] = array(
 $quotes[] = array(
   'quote' => "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
   'source' => 'Homer Simpson',
+  'type' => 'Real Talk ala Mr. Simpson',
   'citation' => 'The Simpsons, Season 9, EP 8',
   'year' => 1997
 
@@ -24,6 +25,7 @@ $quotes[] = array(
 $quotes[] = array(
   'quote' => "I am so smart! I am so smart! S-M-R-T! I mean S-M-A-R-T!",
   'source' => 'Homer Simpson',
+  'type' => 'Motivation ala Mr. Simpson',
   'citation' => 'The Simpsons, Season 5, Ep 3',
   'year' => 1993
 
@@ -31,6 +33,7 @@ $quotes[] = array(
 $quotes[] = array(
   'quote' => "D'oh!",
   'source' => 'Homer Simpson',
+  'type' => 'Classic Mr. Simpson',
   'citation' => 'The Simpsons',
   'year' => ''
 
@@ -39,39 +42,53 @@ $quotes[] = array(
 $quotes[] = array(
   'quote' => "All my life I've had one dream. To achieve my many goals. Mr. Burns has never given me a thumbs up or a 'way to be' or a 'you go girl!'",
   'source' => 'Homer Simpson',
+  'type' => 'Relatable Mr. Simpson',
   'citation' => 'The Simpsons, Season 14, EP 15',
   'year' => 2003
 );
-//randomly select array key and associated quote w/ elements
+
+//select random quote and it's components from array
 function getRandomQuote($quotes){
   global $quotes;
 return $quotes[array_rand($quotes)];
 }
-//display appointed 'quote on deck'. If statements will ensure blank elements are suppressed.;
+//print random quote to page
 function printQuote($quoteOnDeck){
 global $quotes;
 $quoteOnDeck = getRandomQuote($quotes);
 $DisplayQuote = '';
+//if and elseif statements used to detect year and citation
   if($quoteOnDeck['citation'] == Null){
 $DisplayQuote = '<p class="quote">' . $quoteOnDeck['quote'] . '</p>';
 $DisplayQuote .= '<p class="source">' . $quoteOnDeck['source'] .
-   '<span class="year">' . $quoteOnDeck['year'] . '</span>
+  '<span class="type">' . $quoteOnDeck['type'] . '</span>' .
+  '<span class="year">' . $quoteOnDeck['year'] . '</span>
 </p>';
   }elseif($quoteOnDeck['year'] == Null){
 $DisplayQuote = '<p class="quote">' . $quoteOnDeck['quote'] . '</p>';
 $DisplayQuote .= '<p class="source">' . $quoteOnDeck['source'] .
+   '<span class="type">' . $quoteOnDeck['type'] . '</span>' .
    '<span class="citation">' . $quoteOnDeck['citation'] . '</span>
 </p>';
   }elseif($quoteOnDeck['year'] && ['citation'] == Null){
 $DisplayQuote = '<p class="quote">' . $quoteOnDeck['quote'] . '</p>';
-$DisplayQuote .= '<p class="source">' . $quoteOnDeck['source'] . '</p>';
+$DisplayQuote .= '<p class="source">' . $quoteOnDeck['source'] .
+  '<span class="type">' . $quoteOnDeck['type'] . '</span>' .  '</p>';
   }else {
 $DisplayQuote = '<p class="quote">' . $quoteOnDeck['quote'] .'</p>';
 $DisplayQuote .= '<p class="source">' . $quoteOnDeck['source'] .
+  '<span class="type">' . $quoteOnDeck['type'] . '</span>' .
   '<span class="citation">' . $quoteOnDeck['citation'] . '</span>' .
   '<span class="year">' . $quoteOnDeck['year'] . '</span>
 </p>';
   }
 echo $DisplayQuote;
 }
-printQuote($quotes);
+
+//apply random color on refresh
+$colorsArray = array('#3371FF','#062361', '#FF5233', '#64FF33', '#FF33A5');
+
+$randomColor = $colorsArray[array_rand($colorsArray)];
+ return $randomColor;
+
+?>
--- a_random_quote_generator-v1/index.php
+++ a_random_quote_generator-v1/index.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
+<?php include 'quote2.php';?>
 <?php $test = 'Font color and background apply?'; ?>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="10">
   <title>Random Quotes<?php echo $test;?></title>
   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link href="css/normalize.css" rel="stylesheet"  />
   <link href="css/styles.css" rel="stylesheet" />
 </head>
-<body>
+<body style= "background: <?php echo $randomColor; ?>">
   <div class="container">
     <div id="quote-box">
-<?php include 'inc/functions.php';?>
+      <?php printQuote($quotes); ?>
     </div>
-    <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
+
   </div>
 </body>
 </html>
